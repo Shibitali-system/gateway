fff786f291180863266f607512b1e5f4c7aca8f9
Shibitali-system <constantineanatory@gmail.com>
2025-12-26 09:34:27 +0300

    Fix sendtoairpay POST route

--- your_functions.py
+++ your_functions.py
@@ -1,40 +1,36 @@
-import hashlib, base64, json, zlib
+import hashlib
+import base64
+import json
+import zlib
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
 
 class Functions:
-    @staticmethod
-    def encrypt_string(my_string, username, password):
-        secret_key = hashlib.md5((username + "~:~" + password).encode()).hexdigest()
-        iv = 'c0f9e2d16031b0ce'
-        cipher = AES.new(secret_key.encode(), AES.MODE_CBC, iv.encode())
-        encrypted_data = cipher.encrypt(pad(my_string.encode(), AES.block_size))
-        return iv + base64.b64encode(encrypted_data).decode()
 
     @staticmethod
-    def decrypt_string(encrypted_data, username, password):
-        secret_key = hashlib.md5((username + "~:~" + password).encode()).hexdigest()
-        iv = encrypted_data[:16]
-        encrypted_data = encrypted_data[16:]
-        cipher = AES.new(secret_key.encode(), AES.MODE_CBC, iv.encode())
-        decrypted_data = cipher.decrypt(base64.b64decode(encrypted_data))
-        return unpad(decrypted_data, AES.block_size).decode()
+    def encrypt_string(plain_text, username, password):
+        key = hashlib.md5((username + "~:~" + password).encode()).digest()
+        iv = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        encrypted = cipher.encrypt(pad(plain_text.encode(), AES.block_size))
+        return base64.b64encode(iv + encrypted).decode()
 
     @staticmethod
-    def encrypt_sha(data, salt):
-        return hashlib.sha256((salt + '@' + data).encode()).hexdigest()
+    def decrypt_string(enc_text, username, password):
+        raw = base64.b64decode(enc_text)
+        iv = raw[:16]
+        key = hashlib.md5((username + "~:~" + password).encode()).digest()
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = unpad(cipher.decrypt(raw[16:]), AES.block_size)
+        return decrypted.decode()
 
     @staticmethod
-    def checksum_cal(post_data):
-        sorted_data = sorted(post_data.items(), key=lambda x: x[0])
-        data = ''.join([str(v) for _, v in sorted_data])
-        return hashlib.sha256((data).encode()).hexdigest()
+    def encrypt_sha(data, secret):
+        return hashlib.sha256((data + secret).encode()).hexdigest()
 
     @staticmethod
-    def send_post_data(url, post_data):
-        import requests
-        try:
-            response = requests.post(url, data=post_data)
-            return response.text
-        except Exception as e:
-            return json.dumps({"error": str(e)})
+    def checksum_cal(data_dict):
+        raw = "|".join(str(v) for v in data_dict.values() if v)
+        return zlib.crc32(raw.encode()) & 0xffffffff
